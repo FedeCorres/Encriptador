3d02009bc9e212aad4629d3c91c4f8ff5b15b7b3
FedeCorres <fedecorres.07@gmail.com>
2022-04-28 11:19:48 -0300

    Buttons function is added

--- js.js
+++ js.js
@@ -5,47 +5,48 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */
-function encriptador(frase) {
-    var frase = document.getElementsByTagName("textarea")[0].value;
-    var fraseEncriptada = btoa(frase);
-    document.getElementById("textarea-resultado").innerHTML = fraseEncriptada;
+var txtEntrada = document.getElementById("textarea-ingreso");
+var txtSalida = document.getElementById("textarea-resultado");
+function cambiarLetras(cadena) {
+    return cadena
+        .replace(/e/ig, "enter")
+        .replace(/i/ig, "imes")
+        .replace(/a/ig, "ai")
+        .replace(/o/ig, "ober")
+        .replace(/u/ig, "ufat");
+}
+function revLetras(rev) {
+    return rev
+        .replace(/enter/ig, "e")
+        .replace(/imes/ig, "i")
+        .replace(/ai/ig, "a")
+        .replace(/ober/ig, "o")
+        .replace(/ufat/ig, "u");
+}
+function encriptar() {
+    var txt = txtEntrada.value;
+    var nTxt = txt.toLowerCase();
+    var nTn = nTxt.normalize("NFKD");
+    txtSalida.textContent = cambiarLetras(nTn);
 }
 
-/*function encriptador(frase){
-
-    for(let i = 0; i <= length.frase; i++){
-        if(frase[i] == "a"){
-            replace("a", ai);
-        }
-        else if(frase[i] == "e"){
-
-        }
-        else if(frase[i] == "i"){
-
-        }
-        else if(frase[i] == "o"){
-            
-        }
-        else if(frase[i] == "u"){
-            
-        }
-    }
-}   
-function desencriptador(frase){
-
+function desencriptar() {
+    var txt = txtEntrada.value;
+    var nTxt = txt.toLowerCase();
+    var nTn = nTxt.normalize("NFKD");
+    txtSalida.textContent = revLetras(nTn);
 }
-*/
 
-
+// Funcion y variables para copiar resultado
 var buttonCopy = document.querySelector("#copiar");
 var inputResultado = document.querySelector("#textarea-resultado");
 var message = document.querySelector("#message");
 
-buttonCopy.addEventListener("click", function(){
+buttonCopy.addEventListener("click", function () {
     inputResultado.focus();
     document.execCommand("selectAll");
     document.execCommand("copy");
 
     message.innerHTML = "Copiado al portapapeles"
-    setTimeout(()=> message.innerHTML = "", 5000);
+    setTimeout(() => message.innerHTML = "", 5000);
 })
